8ac036d91e9cb061605e76de9d5ff69082eaa1c1
Rai Butera <rai@rbutera.com>
2019-02-25 11:18:43 +0000

    babel and webpack setup successfully

--- src/Navi.js
+++ src/Navi.js
@@ -1,26 +1,30 @@
-import { Component } from "react"
+import React, { Component } from 'react';
 
-class Navi extends Component {
-  constructor(props){
-    const { collapsed = false } = props
+export class Navi extends Component {
+  constructor(props) {
+    super(props);
+    const { collapsed = false } = props;
     this.state = {
       collapsed
-    }
-    super(props)
+    };
   }
 
-  expand: () => {
-    this.setState({ collapsed: false })
-  }
+  expand = () => {
+    this.setState({ collapsed: false });
+  };
 
-  collapse: () => {
-    this.setState({ collapsed: true })
-  }
+  collapse = () => {
+    this.setState({ collapsed: true });
+  };
 
-  render () {
+  render() {
     const { collapsed } = this.state;
-    return <div id="navi" class={collapsed ? 'collapsed' : 'expanded'}>Navi</div>
+    return (
+      <div id="navi" className={collapsed ? 'collapsed' : 'expanded'}>
+        Navi
+      </div>
+    );
   }
 }
 
-export default Navi
+export default Navi;
